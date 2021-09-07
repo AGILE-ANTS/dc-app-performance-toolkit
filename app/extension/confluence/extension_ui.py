16b0d7ea95a96acd8ba6c8cb637dfb13039d1454
martin.kasprzyk <martin.kasprzyk@simpleworks.de>
2021-09-07 17:48:49 +0200

    view profile and view ranking scripts working

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -8,37 +8,37 @@ from selenium_ui.confluence.pages.pages import Login, AllUpdates
 from util.conf import CONFLUENCE_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def badges_view_badges(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    @print_timing("selenium_app_specific_user_login")
-    def measure():
-        def app_specific_user_login(username='admin', password='admin'):
-            login_page = Login(webdriver)
-            login_page.delete_all_cookies()
-            login_page.go_to()
-            login_page.wait_for_page_loaded()
-            login_page.set_credentials(username=username, password=password)
-            login_page.click_login_button()
-            if login_page.is_first_login():
-                login_page.first_user_setup()
-            all_updates_page = AllUpdates(webdriver)
-            all_updates_page.wait_for_page_loaded()
-        app_specific_user_login(username='admin', password='admin')
-    measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_badges_view_badges")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_badges_view_badges:view_profile_page")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/users/viewmyprofile.action")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "my-badges-panel"))  # Wait for you badges panel visible
+            page.wait_until_visible((By.ID, "title-text"))
+            page.wait_until_visible((By.ID, "my-badges-panel"))
+            page.wait_until_visible((By.ID, "my-badges-panel-table-recent-badges"))
+            page.wait_until_visible((By.ID, "my-badges-panel-table-badges"))
+        sub_measure()
+    measure()
+
+def badges_view_ranking(webdriver, datasets):
+    page = BasePage(webdriver)
+
+    @print_timing("selenium_badges_view_ranking")
+    def measure():
+
+        @print_timing("selenium_badges_view_ranking:view_ranking_page")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/badge/viewUserRanking.action")
+            page.wait_until_visible((By.ID, "rank"))
+            page.wait_until_visible((By.ID, "username"))
+            page.wait_until_visible((By.ID, "goldBadges"))
+            page.wait_until_visible((By.ID, "silverBadges"))
+            page.wait_until_visible((By.ID, "bronzeBadges"))
+            page.wait_until_visible((By.ID, "total"))
+            page.wait_until_visible((By.ID, "points"))
         sub_measure()
     measure()
