17958f3d43da464e8d3554f83003d4c27c4a7432
martin.kasprzyk <martin.kasprzyk@simpleworks.de>
2022-01-21 23:49:05 +0100

    script

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -8,37 +8,16 @@ from selenium_ui.confluence.pages.pages import Login, AllUpdates
 from util.conf import CONFLUENCE_SETTINGS
 
 
-def badges_view_badges(webdriver, datasets):
+def rip_view_page_with_macro(webdriver, datasets):
     page = BasePage(webdriver)
 
-    @print_timing("selenium_badges_view_badges")
+    @print_timing("selenium_rip_view_page_with_macro")
     def measure():
 
-        @print_timing("selenium_badges_view_badges:view_profile_page")
+        @print_timing("selenium_rip_view_page_with_macro:view_page")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/users/viewmyprofile.action")
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/display/RIP/RIP")
             page.wait_until_visible((By.ID, "title-text"))
-            page.wait_until_visible((By.ID, "my-badges-panel"))
-            page.wait_until_visible((By.ID, "my-badges-panel-table-recent-badges"))
-            page.wait_until_visible((By.ID, "my-badges-panel-table-badges"))
-        sub_measure()
-    measure()
-
-def badges_view_ranking(webdriver, datasets):
-    page = BasePage(webdriver)
-
-    @print_timing("selenium_badges_view_ranking")
-    def measure():
-
-        @print_timing("selenium_badges_view_ranking:view_ranking_page")
-        def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/badge/viewUserRanking.action")
-            page.wait_until_visible((By.ID, "rank"))
-            page.wait_until_visible((By.ID, "username"))
-            page.wait_until_visible((By.ID, "goldBadges"))
-            page.wait_until_visible((By.ID, "silverBadges"))
-            page.wait_until_visible((By.ID, "bronzeBadges"))
-            page.wait_until_visible((By.ID, "total"))
-            page.wait_until_visible((By.ID, "points"))
+            page.wait_until_visible((By.XPATH, "//table[contains(@id,'rip-table')]"))
         sub_measure()
     measure()
